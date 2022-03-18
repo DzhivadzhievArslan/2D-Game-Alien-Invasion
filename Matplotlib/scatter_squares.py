61031ec30008eed168f77f6d714cfba77c88adcc
DzhivadzhievArslan <arsymaster@gmail.com>
2022-03-18 14:33:40 +0300

    Внесены изменения визуализации случайного блуждения.

--- Matplotlib/scatter_squares.py
+++ Matplotlib/scatter_squares.py
@@ -21,4 +21,4 @@ ax.tick_params(axis='both', which='major', labelsize=14)
 # Назначение диапазона для каждой оси.
 ax.axis([0, 1100, 0, 1100000])
 
-plt.show()
+plt.savefig('squares_plot.png', bbox_inches='tight')
